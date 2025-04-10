83809e947e1cbfa7f2761ad7cf361995ab0ca4fc
ericlaa <207110593+ericlaa@users.noreply.github.com>
2025-04-10 16:21:22 -0700

    Adding Code Journal 1 Prompt 1

---
+++ cj1-prompt.py
@@ -0,0 +1,9 @@
+name = "Eric La"
+pronouns = "he/him"
+fav_movie = "Spider-Man: Into the Spider-Verse"
+fav_food = "Chicken Alfredo Pasta"
+
+print(f"Name: {name}")
+print(f"Pronouns: {pronouns}")
+print(f"Favorite movie: {fav_movie}")
+print(f"Favorite food: {fav_food}")
